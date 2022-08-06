872a4d224e3a07d89a5677cc9b74a9836771c224
miquelTC <miqueltrallero.c@gmail.com>
2022-08-06 12:46:26 +0200

    NftStore: contract created

--- src/contracts/NftStore.sol
+++ src/contracts/NftStore.sol
@@ -5,5 +5,25 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract NftStore {
-    
+  // Buy Mystery Box
+  // Mint NFT
+
+  address public factoryAddress;
+
+  mapping(address => mapping(address => uint)) public mysteryBoxUserCounter;
+  mapping(address => uint) public mysteryBoxCounter;
+
+  constructor(
+    address _factoryAddress
+  )
+  {
+    factoryAddress = _factoryAddress;
+  }
+
+
+  function buyMysteryBox(address collection) external {
+    // require dinero
+    // require MB no excedan el maximo
+
+  }
 }
