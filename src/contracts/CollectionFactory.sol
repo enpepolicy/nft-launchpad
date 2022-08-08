cb997ba2d00db501af7df005aaa0514082123af2
JDPolanco <juandabiada@gmail.com>
2022-08-08 14:54:17 +0200

    CollectionFactory: struct updated

--- src/contracts/CollectionFactory.sol
+++ src/contracts/CollectionFactory.sol
@@ -5,29 +5,38 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-contract NftCollection is ERC721 {  
+contract NftCollection is ERC721 {
+  string baseURI;
   constructor(
     string memory _tokenName,
     string memory _tokenSymbol,
-    uint _presaleDate,
-    uint _mysteryBoxCap,
-    uint _nftCap,
     string memory _baseUri
   )
 
     ERC721(_tokenName, _tokenSymbol)
   {    
-    
+    baseURI = _baseUri;
+  }
+
+  // Override
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+  // Override
+  function supportsInterface(bytes4 interfaceId) public view override(ERC721) returns (bool) {
+    return super.supportsInterface(interfaceId);
   }
 }
 
 contract CollectionFactory {
 
   address[] public collections;
+
   struct Collections {
     uint presaleDate;
-    uint mysteryBoxCap;
-    uint nftCap;
+    uint16 mysteryBoxCap;
+    uint16 nftCap;
+    uint16[] availableNfts;
     address owner;
   }
 
@@ -37,16 +46,19 @@ contract CollectionFactory {
     string memory _tokenName, 
     string memory _tokenSymbol,
     uint _presaleDate,
-    uint _mysteryBoxCap,
-    uint _nftCap,
+    uint16 _mysteryBoxCap,
+    uint16 _nftCap,
     string memory _baseUri
   ) external {
+
+    // Initialize array to later on pick nft to mint
+    uint16[] memory _availableNfts = new uint16[](_nftCap);
+    for(uint16 i = 0; i < _nftCap; i++) {
+      _availableNfts[i] = i;
+    }
     NftCollection _collection = new NftCollection(
       _tokenName,
       _tokenSymbol,
-      _presaleDate,
-      _mysteryBoxCap,
-      _nftCap,
       _baseUri
     );
 
@@ -54,13 +66,26 @@ contract CollectionFactory {
       _presaleDate,
       _mysteryBoxCap,
       _nftCap,
+      _availableNfts,
       msg.sender
     );
-
     collections.push(address(_collection));
   }
 
   function getCollection(address _collection) external view returns(Collections memory) {
     return collection[_collection];
   }
+
+  function updateCollection(address _nftCollection, uint16 _indexToDelete) external {
+    // TODO: require to only minter
+    uint16[] storage _availableNfts = collection[_nftCollection].availableNfts;
+    _availableNfts[_indexToDelete] = _availableNfts[_availableNfts.length - 1];
+    _availableNfts.pop();
+  }
+
 }
+
+/*
+1. Factory
+2. NftStore
+*/
